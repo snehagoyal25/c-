04692d9fd812d2e995e6a3e6850568bc3ff114d6
sneha-mmu <goyalsneha995>
2024-02-13 13:06:27 +0530

    To print table

---
+++ Table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int i;
+    cout<<"Enter a number : "<<endl;
+    cin>>n;
+    for(i=1;i<=10;i++){
+        cout<<n*i<<endl;
+    }
+    return 0;
+}
